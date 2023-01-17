0de95cb5a2efdab2873aeb6f0581537fe1742178
DMG1k <devingalagarza1@gmail.com>
2023-01-17 17:11:36 -0500

    I completed exercises 1-5 and passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    args.forEach(function(arg) {
+        let index = arr.indexOf(arg);
+        while (index !== -1) {
+            arr.splice(index, 1);
+            index = arr.indexOf(arg);
+        }
+    });
+    return arr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
